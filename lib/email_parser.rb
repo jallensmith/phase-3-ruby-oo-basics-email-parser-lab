8d6e01fff8ced59107f2c2348826d09e32877b15
Jerome <jeromeallen99@gmail.com>
2023-05-13 17:17:43 -0400

    commit tree bd0bbf9d82268991440a0d17c2a5d349c7291bbf

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,22 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
+# Build a class EmailAddressParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    # the "/, | /" code below is a regular expression
+    # the beginning and end of a regular expression is defined by //
+    # within the regular expression, we are identifying two possible strings to split on:
+    # ", " or " "
+    # the pipe | represents OR in a regular expression
+    # .uniq is an array method that removes duplicate values from the array it is called on and returns a new array
+    emails.split(/, | /).uniq
+  end
+end
